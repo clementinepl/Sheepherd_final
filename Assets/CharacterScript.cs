0e169afdf3e47f0e6ae9d639689c0e8a5875b74b
Keirhim <clementinepl@live.fr>
2016-02-28 12:26:44 +0100

    Projet

---
+++ Assets/CharacterScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterScript : MonoBehaviour {
+
+    public float vSpeed = 10;
+    public float rSpeed = 10;
+
+    private bool onTheGround = false;
+    private Rigidbody body;
+    private AudioSource son;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+        son = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        float vx = Input.GetAxis("Horizontal")*rSpeed;
+        float vz = Input.GetAxis("Vertical")*vSpeed;
+
+        var v = body.transform.right;
+        v.y = 0.0f;
+        v.Normalize();
+        v *= vz;
+        v.y = body.velocity.y;
+        body.velocity = v;
+        //body.velocity = body.transform.forward * vz;
+        body.transform.localEulerAngles = new Vector3(body.transform.localEulerAngles.x, body.transform.localEulerAngles.y + vx, body.transform.localEulerAngles.z);
+    }
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Minimouton" && son.isPlaying == false)
+        {
+            son.Play();
+        }
+    }
+}
---
+++ Assets/WINScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WINScript : MonoBehaviour {
+    private int mouton=0;
+    private int moutonTotal = 0;
+    private AudioSource son;
+    private bool sonnorepeat = false;
+    private GameObject compteur;
+    // Use this for initialization
+    void Start () {
+        moutonTotal = GameObject.FindGameObjectsWithTag("Minimouton").Length;
+        son = GetComponent<AudioSource>();
+        compteur = GameObject.Find("Compteur");
+        compteur.GetComponentsInChildren<Text>()[1].text = "/" + moutonTotal;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (moutonTotal == mouton) {
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+            if(!son.isPlaying && !sonnorepeat)
+            {
+                son.Play();
+                sonnorepeat = true;
+            }
+               
+        }
+	
+	}
+
+
+void OnTriggerEnter(Collider collider)
+{
+    if (collider.gameObject.tag == "Minimouton")
+    {
+            mouton += 1;
+            compteur.GetComponentsInChildren<Text>()[0].text = mouton.ToString();
+        }
+}
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Minimouton")
+        {
+            mouton -= 1;
+            compteur.GetComponentsInChildren<Text>()[0].text = mouton.ToString();
+        }
+    }
+
+
+
+}
